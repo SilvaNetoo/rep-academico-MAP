c2bafd33bca4996165c8eb401971cf5a91e82f44
Aluno <aluno@cesed.local>
2018-04-24 10:50:22 -0300

    alterando valores de salarios

--- atividade 08/strategy/src/strategy/Main.java
+++ atividade 08/strategy/src/strategy/Main.java
@@ -2,13 +2,13 @@ package strategy;
 
 public class Main {
 	public static void main(String[] args) {
-		Funcionario umFuncionario = new Funcionario(Funcionario.DESENVOLVEDOR,2100, 1);
+		Funcionario umFuncionario = new Funcionario(Funcionario.DESENVOLVEDOR,3500, 1);
 		System.out.println(umFuncionario.calcularGratificacao());
 		
-		Funcionario outroFuncionario = new Funcionario(Funcionario.DBA,1700, 2);
+		Funcionario outroFuncionario = new Funcionario(Funcionario.DBA,3200, 2);
 		System.out.println(outroFuncionario.calcularGratificacao());
 		
-		Funcionario maisUmFuncionario = new Funcionario(Funcionario.GERENTE,1000,4);
+		Funcionario maisUmFuncionario = new Funcionario(Funcionario.GERENTE,5000,4);
 		System.out.println(maisUmFuncionario.calcularGratificacao());
 	}
 }
